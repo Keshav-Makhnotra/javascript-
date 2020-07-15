5cff4c54eea7bf63f66ebc6d1265e5e3c0fd3dcb
Keshav-Makhnotra <68325530+Keshav-Makhnotra@users.noreply.github.com>
2020-07-15 22:24:50 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,42 @@
+// Data types
+
+/* there various types of date types and in js datatypes are dyanamics. That mean same variable can be used to hold the different datatypes */
+// Example
+
+var x;// now x is undeefined
+x=5;// now x is number
+x="john" // now x is string
+
+/* 1 number
+javascript has only one types of number.
+Number can written with or wothout the decimal */
+
+var x1=31; // written without decimal
+var x2=33.33; // written with decimal
+
+/* 2 strings
+strings is the collection of characters like 'keshav'.Strings are written with quotes you can use single or double qoutes as wish. */
+
+var a='John';// with single qoutes
+var a1="john";// with double qoutes 
+
+/* 3 Boolean
+Boolean can only two values : true or false.
+Boolean are used in mainly conditional testing. */
+
+var x=5;
+var y=5;
+var z=6;
+(x==y)//true
+(x==z)//false
+
+/* 4 Array
+javascript arrays are written in bracets[], and elemnys get seperated by comma',' ' 
+the following code declares the array called cars, which containg three items. */
+
+var cars=["audi", "BMW", "jaguar"]
+
+/* 5 Object
+javascript objects are created with braces{}, Objects properties are written as name: value pairs seperated by commas. */
+
+var person{firstname:'keshav',lastname:'Makhnotra', age:20};
