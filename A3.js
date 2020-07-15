56761da73a84c41332834d8250bfd4f76e0c327e
Keshav-Makhnotra <68325530+Keshav-Makhnotra@users.noreply.github.com>
2020-07-16 02:37:02 +0530

    Add files via upload

---
+++ A3.js
@@ -0,0 +1,21 @@
+console.log("hello")
+
+let a=prompt("Are you 21+  type below yes/no")
+//console.log(Boolean (a))
+//b=Boolean(a)
+//console.log( typeof b)
+//if(b=="no")
+let b=0;
+
+if( a=='yes')
+b=1;
+
+if(b==1)
+{
+  console.log("can drink")
+}
+else
+{
+  console.log("cannot drink")
+}
+
