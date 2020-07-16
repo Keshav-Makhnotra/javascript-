024cd903f1e8973115c6383f8f8ab126d94d4fc4
Keshav-Makhnotra <68325530+Keshav-Makhnotra@users.noreply.github.com>
2020-07-17 01:15:59 +0530

    Add files via upload

---
+++ OS.js
@@ -0,0 +1,9 @@
+console.log("hey")
+
+let a = prompt("Enter your's OS and version")
+let b=a.split(" ")
+
+//console.log(a, typeof a)
+//console.log(b)
+
+console.log("The OS name is "+b[0]+" the version is "+b[1])
